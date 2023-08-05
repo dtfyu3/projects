3f7cb3b4c1750379486d9a30ff6b9f5d7906413f
Maxim Bogdanov <126229962+dtfyu3@users.noreply.github.com>
2023-08-05 14:43:43 +0300

    Add files via upload

---
+++ pdf/pdf/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(341, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выберите файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "200",
+            "250",
+            "300",
+            "350",
+            "400",
+            "450",
+            "500",
+            "550",
+            "600"});
+            this.comboBox1.Location = new System.Drawing.Point(341, 235);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(184, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Разрешение";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.MouseHover += new System.EventHandler(this.comboBox1_MouseHover);
+            this.comboBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseMove);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 354);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Файлы PDF|*.pdf";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(341, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 27);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Выберите директорию";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(341, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 27);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Старт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(484, 265);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(41, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(484, 291);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(41, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(338, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Начальная страница";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(338, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Конечная страница";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(531, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ShortcutsEnabled = false;
+            this.textBox3.Size = new System.Drawing.Size(198, 15);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(531, 209);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ShortcutsEnabled = false;
+            this.textBox4.Size = new System.Drawing.Size(198, 15);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(382, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(722, 425);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Сброс";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pdf to images";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ pdf/pdf/Form1.cs
@@ -0,0 +1,242 @@
+﻿using Cyotek.GhostScript.PdfConversion;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace pdf
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            
+            InitializeComponent();
+            //EmbeddedAssembly.Load("MyApp.gsdll32.dll", "gsdll32.dll");
+            this.bwForm = new BackgroundWorker();
+            this.bwForm.DoWork += new DoWorkEventHandler(this.BwForm_DoWork);
+            this.bwForm.ProgressChanged += new ProgressChangedEventHandler(this.BwForm_ProgressChanged);
+            this.bwForm.RunWorkerCompleted += new RunWorkerCompletedEventHandler(this.BwForm_RunWorkerCompleted);
+        }
+        private BackgroundWorker bwForm;
+        Pdf2Image pdf;
+        string outputFileName = "";
+        string path;
+        string i1n = "";
+        int firstpage = 1;
+        int lastpage = 0;
+        int counter = 0;
+        int dpi = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                label3.Text = "";
+                progressBar1.Value = 0;
+                Regex reg = new Regex(@"^([^а-яА-Я]+)$");
+                if (!reg.IsMatch(openFileDialog1.FileName))
+                    MessageBox.Show("Полный путь к файлу не должен содержать кириллицы", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    button2.Enabled = true;
+                    i1n = openFileDialog1.FileName;
+                    textBox3.Text = i1n;
+                    pdf = new Pdf2Image(i1n);
+                    pdf.Settings.ImageFormat = Cyotek.GhostScript.ImageFormat.Png24;
+                    textBox2.Text = $"{pdf.PageCount}";
+                    textBox1.Text = "1";
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dpi = Convert.ToInt32(comboBox1.SelectedItem);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            button2.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Text = "";
+            textBox3.Enabled = false;
+            textBox4.Text = "";
+            textBox4.Enabled = false;
+            comboBox1.Enabled = false;
+            bwForm.WorkerReportsProgress = true;
+            progressBar1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Regex reg = new Regex(@"^([^а-яА-Я]+)$");
+                if (!reg.IsMatch(folderBrowserDialog1.SelectedPath))
+                    MessageBox.Show("Путь к директории не должен содержать кириллицы", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    button3.Enabled = true;
+                    path = folderBrowserDialog1.SelectedPath;
+                    textBox4.Text = path;
+                    textBox1.Enabled = true;
+                    textBox2.Enabled = true;
+                    comboBox1.Enabled = true;
+                }
+            }
+        }
+        void BwForm_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+            counter = firstpage;
+            int y = 1;
+            try
+            {
+                Disable();
+                while (counter <= lastpage)
+                {
+                    outputFileName = path + $@"\{counter}.png";
+                    double d = y * 100 / (lastpage - firstpage + 1);
+                    int per = Convert.ToInt32(d);
+                    pdf.ConvertPdfPageToImage(outputFileName, counter);
+                    bgw.ReportProgress(per);
+                    counter++;
+                    y++;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                DialogResult result = MessageBox.Show($"Возникла ошибка\n{ex.Message}", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Enable();
+            }
+
+        }
+        void Enable()
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            comboBox1.Enabled = true;
+            button4.Enabled = true;
+        }
+
+        void Disable()
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            comboBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+        void BwForm_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        void BwForm_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Enable();
+            MessageBox.Show("Готово", "Сообщение");
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                if (Convert.ToInt32(textBox1.Text) <= Convert.ToInt32(textBox2.Text) && Convert.ToInt32(textBox2.Text) <= pdf.PageCount && Convert.ToInt32(textBox1.Text) != 0 && Convert.ToInt32(textBox2.Text) != 0)
+                {
+
+                    firstpage = Convert.ToInt32(textBox1.Text);
+                    lastpage = Convert.ToInt32(textBox2.Text);
+                    if (dpi != 0)
+                    {
+                        pdf.Settings.Dpi = dpi;
+                        progressBar1.Visible = true;
+                        bwForm.RunWorkerAsync();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Процесс будет запущен со значением dpi по умолчанию - 200", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        bwForm.RunWorkerAsync();
+                    }
+                }
+                else
+                {
+                    textBox1.Text = "1";
+                    textBox2.Text = pdf.PageCount.ToString();
+                    MessageBox.Show("Проверьте правильность ввода");
+                }
+            }
+            else
+            {
+                textBox1.Text = "1";
+                textBox2.Text = pdf.PageCount.ToString();
+                MessageBox.Show("Проверьте правильность ввода");
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //if (textBox1.Text == null || textBox1.Text == "")
+            //{
+            //    textBox1.Text = "1";
+            //}
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //if (textBox2.Text == null || textBox2.Text == "")
+            //{
+            //    textBox2.Text = textBox2.Text = pdf.PageCount.ToString();
+            //}
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void comboBox1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (comboBox1.DropDownStyle != ComboBoxStyle.DropDownList)
+            {
+                comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+                comboBox1.SelectedItem = "300";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
